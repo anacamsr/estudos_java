2dca0bdeb0284294d978cd5f7233b7103dd35168
Carol <anacamsr@gmail.com>
2024-02-14 10:32:42 -0500

    first commit

---
+++ Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println("contasSimples");
+        contasSimples();
+    }
+
+    public static void contasSimples() {
+        int a = 30;
+        int b = 10;
+
+        System.out.println("A soma de a e b é: " + (a + b));
+        System.out.println("A subtração de a e b é: " + (a - b));
+        System.out.println("A divisão de a e b é: " + (a / b));
+        System.out.println("A multiplicação de a e b é: " + (a * b));
+    }
+}
